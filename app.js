e1c835f7a62d6f0bd528c69d5eab50f5a0535dac
rojimark <rmbedana@Roggies-MacBook-Pro.local>
2018-01-31 05:03:42 +0800

    route setup

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
+
+const poll = require('./routes/poll');
 //Set public folder
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,6 +16,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 //Enable cors
 app.use(cors());
+app.use('/poll', poll);
 
 const port = 3000;
 
